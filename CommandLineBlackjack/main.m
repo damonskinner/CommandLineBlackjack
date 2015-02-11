48f84e81c07e2b82f6ca0ccedea777c07dda4534
damonskinner <dcs250@gmail.com>
2015-02-11 10:07:40 -0500

    Initial Commit

---
+++ CommandLineBlackjack/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CommandLineBlackjack
+//
+//  Created by Damon Skinner on 2/11/15.
+//  Copyright (c) 2015 Damon Skinner. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
